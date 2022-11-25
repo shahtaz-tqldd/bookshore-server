94a5cd7762cafe4d93d7be20401faa0f1e6e0009
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-11-25 22:07:48 +0600

    mongodb connected and load data to the client

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,6 +9,25 @@ const books = require('./books.json')
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1uor19o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const collection = client.db("test").collection("devices");
+
+async function run(){
+    try{
+        const productsCollection = client.db('bookShore').collection('products')
+        app.get('/products', async(req, res)=>{
+            const query = {}
+            const result = await productsCollection.find(query).toArray()
+            res.send(result)
+        })
+    }
+    finally{}
+}
+
+run().catch(err=>console.error(err))
+
 
 app.get('/', (req, res)=>{
     res.send('Bookshore server is running...')
