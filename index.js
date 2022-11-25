f8bee46769fd37f6aaef7b37ba47aa290996468b
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-11-25 21:46:11 +0600

    server database connects to client

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const books = require('./books.json')
 
 // middleware
 app.use(cors())
@@ -9,6 +11,9 @@ app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send('Bookshore server is running...')
+})
+app.get('/books', (req, res)=>{
+    res.send(books)
 })
 
 app.listen(port, ()=>{
